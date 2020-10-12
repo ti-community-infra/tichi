f7cefba84c8376a91a7094b32735510619348b21
二手掉包工程师 <rustin.liu@gmail.com>
2020-10-12 18:51:52 +0800

    test: add more cases bb4465a  (#11)
    * test: add more cases
    
    * test: add more cases

--- pkg/externalplugins/config_test.go
+++ pkg/externalplugins/config_test.go
@@ -4,9 +4,11 @@ package externalplugins
 import (
 	"fmt"
 	"testing"
+
+	"gotest.tools/assert"
 )
 
-func TestValidateConfigUpdater(t *testing.T) {
+func TestValidateTiCommunityLgtmConfig(t *testing.T) {
 	testCases := []struct {
 		name     string
 		lgtm     *TiCommunityLgtm
@@ -60,6 +62,111 @@ func TestValidateConfigUpdater(t *testing.T) {
 			if tc.expected != nil && actual != nil && tc.expected.Error() != actual.Error() {
 				t.Errorf("expected error '%v', but it is '%v'", tc.expected, actual)
 			}
+		})
+	}
+}
+
+func TestValidateConfig(t *testing.T) {
+	testCases := []struct {
+		name     string
+		lgtm     *TiCommunityLgtm
+		expected error
+	}{
+		{
+			name: "https pull reviewers URL",
+			lgtm: &TiCommunityLgtm{
+				Repos:            []string{"tidb-community-bots/test-dev"},
+				ReviewActsAsLgtm: true,
+				StoreTreeHash:    true,
+				StickyLgtmTeam:   "tidb-community-bots/bots-test",
+				PullReviewersURL: "https://bots.tidb.io/ti-community-bot",
+			},
+			expected: nil,
+		},
+		{
+			name: "http pull reviewers URL",
+			lgtm: &TiCommunityLgtm{
+				Repos:            []string{"tidb-community-bots/test-dev"},
+				ReviewActsAsLgtm: true,
+				StoreTreeHash:    true,
+				StickyLgtmTeam:   "tidb-community-bots/bots-test",
+				PullReviewersURL: "http://bots.tidb.io/ti-community-bot",
+			},
+			expected: nil,
+		},
+		{
+			name: "invalid pull reviewers URL",
+			lgtm: &TiCommunityLgtm{
+				Repos:            []string{"tidb-community-bots/test-dev"},
+				ReviewActsAsLgtm: true,
+				StoreTreeHash:    true,
+				StickyLgtmTeam:   "tidb-community-bots/bots-test",
+				PullReviewersURL: "http/bots.tidb.io/ti-community-bot",
+			},
+			expected: fmt.Errorf("parse \"http/bots.tidb.io/ti-community-bot\": invalid URI for request"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			config := Configuration{TiCommunityLgtm: []TiCommunityLgtm{
+				*tc.lgtm,
+			}}
+			actual := config.Validate()
+
+			if tc.expected == nil && actual != nil {
+				t.Errorf("unexpected error: '%v'", actual)
+			}
+			if tc.expected != nil && actual == nil {
+				t.Errorf("expected error '%v'', but it is nil", tc.expected)
+			}
+			if tc.expected != nil && actual != nil && tc.expected.Error() != actual.Error() {
+				t.Errorf("expected error '%v', but it is '%v'", tc.expected, actual)
+			}
+		})
+	}
+}
+
+func TestLgtmFor(t *testing.T) {
+	testCases := []struct {
+		name string
+		lgtm *TiCommunityLgtm
+		org  string
+		repo string
+	}{
+		{
+			name: "Full name",
+			lgtm: &TiCommunityLgtm{
+				Repos:            []string{"tidb-community-bots/test-dev"},
+				ReviewActsAsLgtm: true,
+				StoreTreeHash:    true,
+				StickyLgtmTeam:   "tidb-community-bots/bots-test",
+				PullReviewersURL: "https://bots.tidb.io/ti-community-bot",
+			},
+			org:  "tidb-community-bots",
+			repo: "test-dev",
+		},
+		{
+			name: "Only org",
+			lgtm: &TiCommunityLgtm{
+				Repos:            []string{"tidb-community-bots"},
+				ReviewActsAsLgtm: true,
+				StoreTreeHash:    true,
+				StickyLgtmTeam:   "tidb-community-bots/bots-test",
+				PullReviewersURL: "http://bots.tidb.io/ti-community-bot",
+			},
+			org:  "tidb-community-bots",
+			repo: "test-dev",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			config := Configuration{TiCommunityLgtm: []TiCommunityLgtm{
+				*tc.lgtm,
+			}}
+			lgtm := config.LgtmFor(tc.org, tc.repo)
+			assert.DeepEqual(t, lgtm.Repos, tc.lgtm.Repos)
 		})
 	}
 }
