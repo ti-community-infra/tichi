980e152f712334b521c21ca2f613ea09aa56ee95
二手掉包工程师 <rustin.liu@gmail.com>
2020-10-15 13:55:34 +0800

    feat: add approve config (#19)
    * feat: add approve config
    
    * test: add more cases
    
    * test: add more cases

--- pkg/externalplugins/config.go
+++ pkg/externalplugins/config.go
@@ -10,6 +10,7 @@ import (
 // Configuration is the top-level serialization target for external plugin Configuration.
 type Configuration struct {
 	TiCommunityLgtm []TiCommunityLgtm `json:"ti-community-lgtm,omitempty"`
+	Approve         []Approve
 }
 
 // TiCommunityLgtm specifies a configuration for a single ti community lgtm.
@@ -33,6 +34,48 @@ type TiCommunityLgtm struct {
 	PullOwnersURL string `json:"pull_owners_url,omitempty"`
 }
 
+// Approve specifies a configuration for a single approve.
+//
+// The configuration for the approve plugin is defined as a list of these structures.
+type Approve struct {
+	// Repos is either of the form org/repos or just org.
+	Repos []string `json:"repos,omitempty"`
+	// IssueRequired indicates if an associated issue is required for approval in
+	// the specified repos.
+	IssueRequired bool `json:"issue_required,omitempty"`
+	// RequireSelfApproval requires PR authors to explicitly approve their PRs.
+	// Otherwise the plugin assumes the author of the PR approves the changes in the PR.
+	RequireSelfApproval *bool `json:"require_self_approval,omitempty"`
+	// LastLgtmActsAsApprove indicates that the lgtm command should be used to
+	// indicate approval
+	LastLgtmActsAsApprove bool `json:"last_lgtm_acts_as_approve,omitempty"`
+	// IgnoreReviewState causes the approve plugin to ignore the GitHub review state. Otherwise:
+	// * an APPROVE github review is equivalent to leaving an "/approve" message.
+	// * A REQUEST_CHANGES github review is equivalent to leaving an /approve cancel" message.
+	IgnoreReviewState *bool `json:"ignore_review_state,omitempty"`
+	// CommandHelpLink is the link to the help page which shows the available commands for each repo.
+	// The command help page is served by Deck and available under https://<deck-url>/command-help.
+	CommandHelpLink string `json:"commandHelpLink,omitempty"`
+	// PrProcessLink is the link to the help page which explains the code review process.
+	PrProcessLink string `json:"pr_process_link,omitempty"`
+	// PullOwnersURL specifies the URL of the reviewer of pull request.
+	PullOwnersURL string `json:"pull_owners_url,omitempty"`
+}
+
+func (a Approve) HasSelfApproval() bool {
+	if a.RequireSelfApproval != nil {
+		return !*a.RequireSelfApproval
+	}
+	return true
+}
+
+func (a Approve) ConsiderReviewState() bool {
+	if a.IgnoreReviewState != nil {
+		return !*a.IgnoreReviewState
+	}
+	return true
+}
+
 // LgtmFor finds the Lgtm for a repo, if one exists
 // a trigger can be listed for the repo itself or for the
 // owning organization
@@ -54,9 +97,50 @@ func (c *Configuration) LgtmFor(org, repo string) *TiCommunityLgtm {
 	return &TiCommunityLgtm{}
 }
 
+// ApproveFor finds the Approve for a repo, if one exists.
+// Approval configuration can be listed for a repository
+// or an organization.
+func (c *Configuration) ApproveFor(org, repo string) *Approve {
+	fullName := fmt.Sprintf("%s/%s", org, repo)
+
+	a := func() *Approve {
+		// First search for repo config
+		for _, approve := range c.Approve {
+			if !sets.NewString(approve.Repos...).Has(fullName) {
+				continue
+			}
+			return &approve
+		}
+
+		// If you don't find anything, loop again looking for an org config
+		for _, approve := range c.Approve {
+			if !sets.NewString(approve.Repos...).Has(org) {
+				continue
+			}
+			return &approve
+		}
+
+		// Return an empty config, and use plugin defaults
+		return &Approve{}
+	}()
+	if a.CommandHelpLink == "" {
+		// TODO: use tidb community deck url.
+		a.CommandHelpLink = "https://go.k8s.io/bot-commands"
+	}
+	if a.PrProcessLink == "" {
+		// TODO: use tidb community url.
+		a.PrProcessLink = "https://git.k8s.io/community/contributors/guide/owners.md#the-code-review-process"
+	}
+	return a
+}
+
 // Validate will return an error if there are any invalid external plugin config.
 func (c *Configuration) Validate() error {
 	if err := validateLgtm(c.TiCommunityLgtm); err != nil {
+		return err
+	}
+
+	if err := validateApprove(c.Approve); err != nil {
 		return err
 	}
 
@@ -67,6 +151,18 @@ func (c *Configuration) Validate() error {
 func validateLgtm(lgtms []TiCommunityLgtm) error {
 	for _, lgtm := range lgtms {
 		_, err := url.ParseRequestURI(lgtm.PullOwnersURL)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// validateApprove will return an error if the URL configured by approve is invalid.
+func validateApprove(approves []Approve) error {
+	for _, approve := range approves {
+		_, err := url.ParseRequestURI(approve.PullOwnersURL)
 		if err != nil {
 			return err
 		}
