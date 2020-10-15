d24ac2d3d3bc4069739d0f49e351697c3307214c
Rustin-Liu <rustin.liu@gmail.com>
2020-10-15 18:56:04 +0800

    fix: rename approve to merge

--- pkg/externalplugins/config.go
+++ pkg/externalplugins/config.go
@@ -9,8 +9,8 @@ import (
 
 // Configuration is the top-level serialization target for external plugin Configuration.
 type Configuration struct {
-	TiCommunityLgtm []TiCommunityLgtm `json:"ti-community-lgtm,omitempty"`
-	Approve         []Approve
+	TiCommunityLgtm []TiCommunityLgtm  `json:"ti-community-lgtm,omitempty"`
+	Merge           []TiCommunityMerge `json:"ti-community-merge,omitempty"`
 }
 
 // TiCommunityLgtm specifies a configuration for a single ti community lgtm.
@@ -18,7 +18,7 @@ type Configuration struct {
 type TiCommunityLgtm struct {
 	// Repos is either of the form org/repos or just org.
 	Repos []string `json:"repos,omitempty"`
-	// ReviewActsAsLgtm indicates that a GitHub review of "approve" or "request changes"
+	// ReviewActsAsLgtm indicates that a GitHub review of "merge" or "request changes"
 	// acts as adding or removing the lgtm label.
 	ReviewActsAsLgtm bool `json:"review_acts_as_lgtm,omitempty"`
 	// StoreTreeHash indicates if tree_hash should be stored inside a comment to detect
@@ -34,46 +34,17 @@ type TiCommunityLgtm struct {
 	PullOwnersURL string `json:"pull_owners_url,omitempty"`
 }
 
-// Approve specifies a configuration for a single approve.
+// TiCommunityMerge specifies a configuration for a single merge.
 //
-// The configuration for the approve plugin is defined as a list of these structures.
-type Approve struct {
+// The configuration for the merge plugin is defined as a list of these structures.
+type TiCommunityMerge struct {
 	// Repos is either of the form org/repos or just org.
 	Repos []string `json:"repos,omitempty"`
-	// IssueRequired indicates if an associated issue is required for approval in
-	// the specified repos.
-	IssueRequired bool `json:"issue_required,omitempty"`
-	// RequireSelfApproval requires PR authors to explicitly approve their PRs.
-	// Otherwise the plugin assumes the author of the PR approves the changes in the PR.
-	RequireSelfApproval *bool `json:"require_self_approval,omitempty"`
-	// LastLgtmActsAsApprove indicates that the lgtm command should be used to
-	// indicate approval
-	LastLgtmActsAsApprove bool `json:"last_lgtm_acts_as_approve,omitempty"`
-	// IgnoreReviewState causes the approve plugin to ignore the GitHub review state. Otherwise:
-	// * an APPROVE github review is equivalent to leaving an "/approve" message.
-	// * A REQUEST_CHANGES github review is equivalent to leaving an /approve cancel" message.
-	IgnoreReviewState *bool `json:"ignore_review_state,omitempty"`
-	// CommandHelpLink is the link to the help page which shows the available commands for each repo.
-	// The command help page is served by Deck and available under https://<deck-url>/command-help.
-	CommandHelpLink string `json:"commandHelpLink,omitempty"`
-	// PrProcessLink is the link to the help page which explains the code review process.
-	PrProcessLink string `json:"pr_process_link,omitempty"`
+	// StoreTreeHash indicates if tree_hash should be stored inside a comment to detect
+	// squashed commits before removing can merge labels.
+	StoreTreeHash bool `json:"store_tree_hash,omitempty"`
 	// PullOwnersURL specifies the URL of the reviewer of pull request.
 	PullOwnersURL string `json:"pull_owners_url,omitempty"`
-}
-
-func (a Approve) HasSelfApproval() bool {
-	if a.RequireSelfApproval != nil {
-		return !*a.RequireSelfApproval
-	}
-	return true
-}
-
-func (a Approve) ConsiderReviewState() bool {
-	if a.IgnoreReviewState != nil {
-		return !*a.IgnoreReviewState
-	}
-	return true
 }
 
 // LgtmFor finds the Lgtm for a repo, if one exists
@@ -97,41 +68,25 @@ func (c *Configuration) LgtmFor(org, repo string) *TiCommunityLgtm {
 	return &TiCommunityLgtm{}
 }
 
-// ApproveFor finds the Approve for a repo, if one exists.
-// Approval configuration can be listed for a repository
+// MergeFor finds the TiCommunityMerge for a repo, if one exists.
+// TiCommunityMerge configuration can be listed for a repository
 // or an organization.
-func (c *Configuration) ApproveFor(org, repo string) *Approve {
+func (c *Configuration) MergeFor(org, repo string) *TiCommunityMerge {
 	fullName := fmt.Sprintf("%s/%s", org, repo)
-
-	a := func() *Approve {
-		// First search for repo config
-		for _, approve := range c.Approve {
-			if !sets.NewString(approve.Repos...).Has(fullName) {
-				continue
-			}
-			return &approve
+	for _, lgtm := range c.Merge {
+		if !sets.NewString(lgtm.Repos...).Has(fullName) {
+			continue
 		}
-
-		// If you don't find anything, loop again looking for an org config
-		for _, approve := range c.Approve {
-			if !sets.NewString(approve.Repos...).Has(org) {
-				continue
-			}
-			return &approve
+		return &lgtm
+	}
+	// If you don't find anything, loop again looking for an org config
+	for _, lgtm := range c.Merge {
+		if !sets.NewString(lgtm.Repos...).Has(org) {
+			continue
 		}
-
-		// Return an empty config, and use plugin defaults
-		return &Approve{}
-	}()
-	if a.CommandHelpLink == "" {
-		// TODO: use tidb community deck url.
-		a.CommandHelpLink = "https://go.k8s.io/bot-commands"
+		return &lgtm
 	}
-	if a.PrProcessLink == "" {
-		// TODO: use tidb community url.
-		a.PrProcessLink = "https://git.k8s.io/community/contributors/guide/owners.md#the-code-review-process"
-	}
-	return a
+	return &TiCommunityMerge{}
 }
 
 // Validate will return an error if there are any invalid external plugin config.
@@ -140,7 +95,7 @@ func (c *Configuration) Validate() error {
 		return err
 	}
 
-	if err := validateApprove(c.Approve); err != nil {
+	if err := validateMerge(c.Merge); err != nil {
 		return err
 	}
 
@@ -159,10 +114,10 @@ func validateLgtm(lgtms []TiCommunityLgtm) error {
 	return nil
 }
 
-// validateApprove will return an error if the URL configured by approve is invalid.
-func validateApprove(approves []Approve) error {
-	for _, approve := range approves {
-		_, err := url.ParseRequestURI(approve.PullOwnersURL)
+// validateMerge will return an error if the URL configured by merge is invalid.
+func validateMerge(merges []TiCommunityMerge) error {
+	for _, merge := range merges {
+		_, err := url.ParseRequestURI(merge.PullOwnersURL)
 		if err != nil {
 			return err
 		}
