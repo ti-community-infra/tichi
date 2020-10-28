2793f4f18d799ffcbde72075990d00781498c3f3
Rustin-Liu <rustin.liu@gmail.com>
2020-10-28 17:01:16 +0800

    test: add more cases

--- internal/pkg/externalplugins/owners/types.go
+++ internal/pkg/externalplugins/owners/types.go
@@ -2,33 +2,33 @@ package owners
 
 // SigResponse specifies the response to the request to get owners.
 type SigResponse struct {
-	Data    SigInfo `json:"data"`
-	Message string  `json:"message"`
+	Data    SigInfo `json:"data,omitempty"`
+	Message string  `json:"message,omitempty"`
 }
 
 // ContributorInfo specifies the contributor's github info.
 type ContributorInfo struct {
 	GithubName string `json:"githubName"`
 	// Level specifies the level of contributor at this sig.
-	Level string `json:"level"`
-	Email string `json:"email"`
+	Level string `json:"level,omitempty"`
+	Email string `json:"email,omitempty"`
 }
 
 // ContributorInfo specifies the sig's membership.
 type SigMembership struct {
-	TechLeaders        []ContributorInfo `json:"techLeaders"`
-	CoLeaders          []ContributorInfo `json:"coLeaders"`
-	Committers         []ContributorInfo `json:"committers"`
-	Reviewers          []ContributorInfo `json:"reviewers"`
-	ActiveContributors []ContributorInfo `json:"activeContributors"`
+	TechLeaders        []ContributorInfo `json:"techLeaders,omitempty"`
+	CoLeaders          []ContributorInfo `json:"coLeaders,omitempty"`
+	Committers         []ContributorInfo `json:"committers,omitempty"`
+	Reviewers          []ContributorInfo `json:"reviewers,omitempty"`
+	ActiveContributors []ContributorInfo `json:"activeContributors,omitempty"`
 }
 
 // ContributorInfo specifies the sig info.
 type SigInfo struct {
 	// Name specifies the name of sig.
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 	// Membership specifies the membership of sig.
-	Membership SigMembership `json:"membership"`
+	Membership SigMembership `json:"membership,omitempty"`
 	// Membership specifies the default required lgtm number of sig.
-	NeedsLgtm int `json:"needsLGTM"`
+	NeedsLgtm int `json:"needsLGTM,omitempty"`
 }
