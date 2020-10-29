89ca9a2f43d9cc2b81ffa11f25ca4e8f189d31bd
Rustin <rustin.liu@gmail.com>
2020-10-29 16:15:34 +0800

    fix: add default sig name (#74)
    * fix: add default sig name
    
    * fix: fmt
    
    * test: add cases

--- internal/pkg/externalplugins/config.go
+++ internal/pkg/externalplugins/config.go
@@ -57,6 +57,9 @@ type TiCommunityOwners struct {
 	Repos []string `json:"repos,omitempty"`
 	// SigEndpoint specifies the URL of the sig info.
 	SigEndpoint string `json:"sig_endpoint,omitempty"`
+	// DefaultSigName specifies the default sig name of this repo's PR.
+	DefaultSigName  string `json:"default_sig_name,omitempty"`
+	OwnersTrustTeam string `json:"trusted_team_for_owners,omitempty"`
 }
 
 // TiCommunityLabel is the config for the label plugin.
