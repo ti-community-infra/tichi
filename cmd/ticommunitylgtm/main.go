c738675ed06c5ccf5cda2364dc7f3880e66a6cfa
hi-rustin <rustin.liu@gmail.com>
2020-11-20 11:16:56 +0800

    fix: lgtm

--- cmd/ticommunitylgtm/main.go
+++ cmd/ticommunitylgtm/main.go
@@ -111,7 +111,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", server)
 
-	helpProvider := lgtm.HelpProvider(epa.Config())
+	helpProvider := lgtm.HelpProvider(epa)
 	externalplugins.ServeExternalPluginHelp(mux, log, helpProvider)
 	httpServer := &http.Server{Addr: ":" + strconv.Itoa(o.port), Handler: mux}
 
