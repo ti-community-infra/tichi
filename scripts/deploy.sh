858ab9e40d89a9192026b6c25f424378e1fda9ca
二手掉包工程师 <rustin.liu@gmail.com>
2020-11-29 21:41:01 +0800

    ci: add deploy job (#173)
    * ci: add deploy job
    
    * ci: add validate job
    
    * ci: add deploy job
    
    * ci: add deploy job
    
    * config: add ti-community-prow-bot service accounts
    
    * config: fix typo
    
    * config: fix typo
    
    * config: test
    
    * config: test
    
    * config: test
    
    * config: test
    
    * config: test
    
    * config: test
    
    * config: test
    
    * config: use deployer-credentials
    
    * config: test
    
    * config: test
    
    * config: test
    
    * config: remove
    
    * config: job
    
    * config: test
    
    * config: test
    
    * config: test

---
+++ scripts/deploy.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ -n "${GOOGLE_APPLICATION_CREDENTIALS:-}" ]]; then
+  echo "Detected GOOGLE_APPLICATION_CREDENTIALS, activating..." >&2
+  gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+fi
+
+case "${1:-}" in
+"--confirm")
+  shift
+  ;;
+*)
+  read -p "Deploy prow to dev [no]: " confirm
+  if [[ "${confirm}" != y* ]]; then
+    exit 1
+  fi
+  ;;
+esac
+
+# See https://misc.flogisoft.com/bash/tip_colors_and_formatting
+color-green() { # Green
+  echo -e "\x1B[1;32m${*}\x1B[0m"
+}
+
+color-context() { # Bold blue
+  echo -e "\x1B[1;34m${*}\x1B[0m"
+}
+
+color-missing() { # Yellow
+  echo -e "\x1B[1;33m${*}\x1B[0m"
+}
+
+ensure-context() {
+  local proj=$1
+  local zone=$2
+  local cluster=$3
+  local context="gke_${proj}_${zone}_${cluster}"
+  echo -n " $(color-context "$context")"
+  kubectl config get-contexts "$context" &> /dev/null && return 0
+  echo ": $(color-missing MISSING), getting credentials..."
+  gcloud container clusters get-credentials --project="$proj" --zone="$zone" "$cluster"
+  kubectl config get-contexts "$context" > /dev/null
+  echo -n "Ensuring contexts exist:"
+}
+
+echo -n "Ensuring dev context exist:"
+current_context=$(kubectl config current-context 2>/dev/null || true)
+restore-context() {
+  if [[ -n "$current_context" ]]; then
+    kubectl config set-context "$current_context"
+  fi
+}
+trap restore-context EXIT
+ensure-context pingcap-testing-account us-central1-c prow-dev
+echo " $(color-green 'done'), Deploying prow..."
+for s in {5..1}; do
+    echo -n $'\r'"in $s..."
+    sleep 1s
+done
+
+# Apply
+cd ./configs/prow-dev/
+make cluster
+echo "$(color-green 'SUCCESS'), Deployed"
