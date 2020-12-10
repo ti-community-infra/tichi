7fb126839c0adc2942b84b9505a6690dbe99aaff
mini256 <minianter@foxmail.com>
2020-12-10 11:18:31 +0800

    fix: add period for notes

--- internal/pkg/externalplugins/label.go
+++ internal/pkg/externalplugins/label.go
@@ -1,12 +1,12 @@
 package externalplugins
 
 const (
-	// CanMergeLabel is the name of the merge label applied by the merge plugin
+	// CanMergeLabel is the name of the merge label applied by the merge plugin.
 	CanMergeLabel = "status/can-merge"
 )
 
 const (
-	// LgtmLabelPrefix is the name of the lgtm label applied by the lgtm plugin
+	// LgtmLabelPrefix is the name of the lgtm label applied by the lgtm plugin.
 	LgtmLabelPrefix = "status/LGT"
 
 	// SigPrefix is a default sig label prefix.
