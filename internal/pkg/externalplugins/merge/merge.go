e9c662373a17a11f115e629743c20d9bd7f32694
hi-rustin <rustin.liu@gmail.com>
2020-12-18 12:22:04 +0800

    address comment

--- internal/pkg/externalplugins/merge/merge.go
+++ internal/pkg/externalplugins/merge/merge.go
@@ -338,7 +338,7 @@ func handle(wantMerge bool, config *externalplugins.Configuration, rc reviewCtx,
 				return strings.Contains(comment.Body, removeCanMergeLabelNoti)
 			})
 		} else {
-			resp := fmt.Sprintf("`/merge` in this pull request requires %d `/lgtm`", owners.NeedsLgtm)
+			resp := fmt.Sprintf("`/merge` in this pull request requires %d `/lgtm`.", owners.NeedsLgtm)
 			log.Infof("Reply /merge request with comment: \"%s\"", resp)
 			return gc.CreateComment(org, repoName, number, externalplugins.FormatResponseRaw(body, htmlURL, author, resp))
 		}
