a7f67d549cd0541dde9ce411aa0203d9799bb8d2
hi-rustin <rustin.liu@gmail.com>
2021-01-21 19:03:26 +0800

    chore: fetch label first

--- internal/pkg/externalplugins/lgtm/lgtm.go
+++ internal/pkg/externalplugins/lgtm/lgtm.go
@@ -280,6 +280,10 @@ func handle(wantLGTM bool, config *externalplugins.Configuration, rc reviewCtx,
 		return gc.CreateComment(org, repo, number, externalplugins.FormatResponseRaw(body, htmlURL, author, resp))
 	}
 
+	labels, err := gc.GetIssueLabels(org, repo, number)
+	if err != nil {
+		return fetchErr("issue labels", err)
+	}
 	botUserChecker, err := gc.BotUserChecker()
 	if err != nil {
 		return fetchErr("bot name", err)
@@ -300,11 +304,6 @@ func handle(wantLGTM bool, config *externalplugins.Configuration, rc reviewCtx,
 
 	// Now we update the LGTM labels, having checked all cases where changing.
 	// Only add the label if it doesn't have it, and vice versa.
-	labels, err := gc.GetIssueLabels(org, repo, number)
-	if err != nil {
-		return fetchErr("issue labels", err)
-	}
-
 	currentLabel, nextLabel := getCurrentAndNextLabel(externalplugins.LgtmLabelPrefix, labels,
 		reviewersAndNeedsLGTM.NeedsLgtm)
 	// Remove the label if necessary, we're done after this.
