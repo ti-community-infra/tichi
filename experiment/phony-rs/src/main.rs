2c74ea5df8d19299d01b69eb6d3f350db58b04be
Chojan Shang <psiace@outlook.com>
2021-01-21 16:34:53 +0800

    feat: async phony-rs

--- experiment/phony-rs/src/main.rs
+++ experiment/phony-rs/src/main.rs
@@ -1,37 +1,32 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+use std::convert::Infallible;
 
-#[macro_use]
-extern crate rocket;
 #[macro_use]
 extern crate serde_derive;
 
 use hmac::{Hmac, Mac, NewMac};
-use rocket::http::Status;
-use rocket::response::status;
-use rocket_contrib::json::Json;
-use rocket_contrib::serve::StaticFiles;
 use sha1::Sha1;
+use warp::{self, Filter};
 
 // Create alias for HMAC-SHA1.
 type HmacSha1 = Hmac<Sha1>;
 
-const SHA1_PREFIX: &'static str = "sha1";
+const SHA1_PREFIX: &str = "sha1";
 
 #[derive(Deserialize)]
-struct Event {
-    address: String,
-    event: String,
-    hmac: String,
-    payload: String,
+pub struct Event {
+    pub address: String,
+    pub event: String,
+    pub hmac: String,
+    pub payload: String,
 }
 
-fn send_hook(
+async fn send_hook(
     address: &str,
     event_type: &str,
     hmac: &str,
     payload: &str,
 ) -> Result<String, Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
     let resp = client
         .post(address)
         .body(payload.to_owned())
@@ -42,13 +37,14 @@ fn send_hook(
             sign_payload(payload.as_bytes(), hmac.as_bytes()),
         )
         .header("Content-Type", "application/json")
-        .send()?;
+        .send()
+        .await?;
     if resp.status().is_success() {
-        Ok(resp.text()?)
+        Ok(resp.text().await?)
     } else {
         Ok(format!(
             "Send event success but something wrong: {}",
-            resp.text()?
+            resp.text().await?
         ))
     }
 }
@@ -64,20 +60,34 @@ fn sign_payload(payload: &[u8], key: &[u8]) -> String {
     signature
 }
 
-#[post("/send", data = "<event>")]
-fn send(event: Json<Event>) -> status::Custom<String> {
-    let result = send_hook(&event.address, &event.event, &event.hmac, &event.payload);
+pub async fn send_event(event: Event) -> Result<impl warp::Reply, Infallible> {
+    let result = send_hook(&event.address, &event.event, &event.hmac, &event.payload).await;
     match result {
-        Err(e) => status::Custom(Status::InternalServerError, e.to_string()),
-        Ok(result) => status::Custom(Status::Ok, result),
+        Err(e) => Ok(warp::reply::json(&e.to_string())),
+        Ok(result) => Ok(warp::reply::json(&result)),
     }
 }
 
-fn main() {
-    rocket::ignite()
-        .mount("/", StaticFiles::from("static"))
-        .mount("/events", routes![send])
-        .launch();
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let log = warp::log("api::request");
+
+    let files = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::dir("static"));
+
+    let message = warp::path!("events" / "send")
+        .and(warp::post())
+        .and(warp::body::form())
+        .and_then(send_event);
+
+    let routes = files.or(message);
+
+    warp::serve(routes.with(log))
+        .run(([127, 0, 0, 1], 8000))
+        .await;
 }
 
 #[cfg(test)]
