9b1fe575ee5aef7363672d0ba51b891a173d01f2
mini256 <minianter@foxmail.com>
2021-02-19 18:31:09 +0800

    feat: add external plugin config checker

---
+++ cmd/checkpluginconfig/main_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"reflect"
+	"testing"
+)
+
+func TestOptions(t *testing.T) {
+	testcases := []struct {
+		name string
+		args []string
+
+		expectedError  string
+		expectedOption *options
+	}{
+		{
+			name: "no external config path",
+			args: []string{},
+
+			expectedError:  "invalid options: required flag --external-plugin-config-path was unset",
+			expectedOption: nil,
+		},
+		{
+			name: "has external config path",
+			args: []string{
+				"--external-plugin-config-path=/etc/external_plugin_config.yaml",
+			},
+
+			expectedError: "",
+			expectedOption: &options{
+				externalPluginConfigPath: "/etc/external_plugin_config.yaml",
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			flags := flag.NewFlagSet(tc.name, flag.ContinueOnError)
+			var actualOptions options
+
+			err := actualOptions.gatherOptions(flags, tc.args)
+
+			if err != nil {
+				if err.Error() != tc.expectedError {
+					t.Errorf("expected error %#v but got %#v", tc.expectedError, err.Error())
+				}
+			} else {
+				if !reflect.DeepEqual(&actualOptions, tc.expectedOption) {
+					t.Errorf("expected options %#v but got %#v", tc.expectedOption, actualOptions)
+				}
+			}
+		})
+	}
+}
+
+func TestValidate(t *testing.T) {
+	testCases := []struct {
+		name string
+		opts options
+	}{
+		{
+			name: "combined config",
+			opts: options{
+				externalPluginConfigPath: "testdata/external_plugins_config.yaml",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if err := validate(tc.opts); err != nil {
+				t.Fatalf("validation failed: %v", err)
+			}
+		})
+	}
+}
