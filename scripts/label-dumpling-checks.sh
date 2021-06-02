95f0e431ea6153ebad9c92613667db1bea209a51
hi-rustin <rustin.liu@gmail.com>
2021-06-02 22:47:10 +0800

    test: add checks
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- scripts/label-dumpling-checks.sh
+++ scripts/label-dumpling-checks.sh
@@ -7,6 +7,11 @@ cd ./tools/label-dumpling/ || exit
 rustup component add rustfmt
 rustup component add clippy
 
+# In the CI environment we need to copy the .env file for use by dotenv
+if [ -z $CI ]; then
+  cp ./tools/label-dumpling/.env.example ./tools/label-dumpling/.env
+fi
+
 # checks
 cargo fmt --all -- --check
 cargo check --all --all-targets
