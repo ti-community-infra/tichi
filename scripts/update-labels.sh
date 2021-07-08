429b2b29f1bce10d8acc5f471660b2ae0a237737
hi-rustin <rustin.liu@gmail.com>
2021-07-08 17:38:06 +0800

    feat: support generate labels.md
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

---
+++ scripts/update-labels.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Build label_sync..." >&2
+
+# Add -mod=mod flag for go build because the default behavior of go build have changed in go 1.16.
+# Refer: https://github.com/golang/go/issues/44212#issuecomment-776937327
+go build -mod=mod -o tools/bin/label_sync k8s.io/test-infra/label_sync
+
+label_sync=$PWD/tools/bin/label_sync
+
+cd configs/prow-dev/config || exit
+
+"$label_sync" \
+  --config=labels.yaml \
+  --action=docs \
+  --docs-template=labels.md.tmpl \
+  --docs-output=labels.md
