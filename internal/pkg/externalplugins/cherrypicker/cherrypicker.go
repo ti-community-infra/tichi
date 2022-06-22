e1c467055f87ad64c3ed0363c9d814cf02e3f21c
wuhuizuo <wuhuizuo@126.com>
2022-06-22 19:59:39 +0800

    [SKIP-CI] config(prow-dev): add blunderbuss plugin (#969)
    * config(prow-dev): add blunderbuss plugin
    
    * style: fix golang ci lint issues
    
    they are correct, but I do the job to avoid golang ci lint wrong reporting.
    
    JIRA: EE-2228

--- internal/pkg/externalplugins/cherrypicker/cherrypicker.go
+++ internal/pkg/externalplugins/cherrypicker/cherrypicker.go
@@ -40,7 +40,7 @@ import (
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/test-infra/prow/config"
-	"k8s.io/test-infra/prow/git/v2"
+	git "k8s.io/test-infra/prow/git/v2"
 	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/pluginhelp"
 	"k8s.io/test-infra/prow/pluginhelp/externalplugins"
